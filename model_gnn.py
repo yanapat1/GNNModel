fb975a0b3893efbd2320b626885cc34418a8c072
foview.work <yanapat.work@gmail.com>
2023-10-03 14:22:01 +0700

    write some new code and deleted stupid codes

--- model_gnn.py
+++ model_gnn.py
@@ -1,3 +1,23 @@
-print('Hello')
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import openpyxl
+
+print('This is a test github')
 for i in range(10):
-    print(i)
+    if i == 5:
+        print('WTF')
+    else:
+        pass
+
+while True:
+    break
+
+def google(g):
+    if g == 'google':
+        q = 2
+    else:
+        q = 1
+    return q
+
