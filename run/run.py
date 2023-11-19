a8c71ada5b756dff91cf57aa2dfb958a2a17eb2a
yanapat1 <yanapat.work@gmail.com>
2023-11-19 19:38:18 +0700

    change

---
+++ run/run.py
@@ -0,0 +1,14 @@
+# from models.models import create_dataframe
+
+# q = {
+#     'id' : ['1','2','3','4'],
+#     'number' : [123,345,567,789]
+# }
+
+# w = create_dataframe(q)
+# print(w)
+
+import os
+import sys
+
+print(os.getcwd())
