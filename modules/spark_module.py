1af8273bf5da127bd3b4642ebaa30da3ef07d22b
yanapat1 <yanapat.work@gmail.com>
2023-11-19 19:46:05 +0700

    chagne spark master

--- modules/spark_module.py
+++ modules/spark_module.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
 def create_spark_session():
-    spark = SparkSession.builder.master('foview').appName('gnn').getOrCreate()
+    spark = SparkSession.builder.master('local[*]').appName('gnn').getOrCreate()
     return spark
